84b5bfaf6457b5af9e15958c26700bb60b1a7875
Lukas Plank <lupl@tuta.io>
2024-08-06 13:58:09 +0200

    chore: Refactor adapter according to newly introduced sparql module
    Replace formerly class-bound functionality with separated functions
    from sparql module.
    
    This also results in a general clean and reorderings.

--- rdfproxy/adapter.py
+++ rdfproxy/adapter.py
@@ -12,7 +12,14 @@ from rdfproxy.utils._exceptions import UndefinedBindingException
 from rdfproxy.utils._types import _TModelInstance
 from rdfproxy.utils.models import Page
 from rdfproxy.utils.sparql.sparql_templates import ungrouped_pagination_base_query
-from rdfproxy.utils.sparql.sparql_utils import calculate_offset, construct_count_query
+from rdfproxy.utils.sparql.sparql_utils import (
+    calculate_offset,
+    construct_count_query,
+    construct_grouped_count_query,
+    construct_grouped_pagination_query,
+    init_sparql_wrapper,
+    sparql_wrapper_query,
+)
 from rdfproxy.utils.utils import (
     get_bindings_from_query_result,
     instantiate_model_from_kwargs,
@@ -66,38 +73,40 @@ class SPARQLModelAdapter(Generic[_TModelInstance]):
         self._query = query
         self._model = model
 
-        self.sparql_wrapper = self._init_sparql_wrapper()
+        self.sparql_wrapper = init_sparql_wrapper(endpoint, query)
 
-    def _init_sparql_wrapper(self) -> SPARQLWrapper:
-        """Initialize a SPARQLWrapper object."""
-        sparql_wrapper = SPARQLWrapper(self._endpoint)
-        sparql_wrapper.setQuery(self._query)
-        sparql_wrapper.setReturnFormat(JSON)
-
-        return sparql_wrapper
-
-    def _run_query(self) -> Iterator[tuple[_TModelInstance, dict[str, Any]]]:
+    def _run_query(
+        self, query: str | None = None
+    ) -> Iterator[tuple[_TModelInstance, dict[str, Any]]]:
         """Run the initially defined query against the endpoint using SPARQLWrapper.
 
         Model instances are coupled with flat SPARQL result bindings;
         this allows for easier and more efficient grouping operations (see query_group_by).
         """
-        query_result: QueryResult = self.sparql_wrapper.query()
+        if query is None:
+            query_result: QueryResult = self.sparql_wrapper.query()
+        else:
+            with temporary_query_override(self.sparql_wrapper):
+                self.sparql_wrapper.setQuery(query)
+                query_result: QueryResult = self.sparql_wrapper.query()
+
         _bindings = get_bindings_from_query_result(query_result)
 
         for bindings in _bindings:
             model = instantiate_model_from_kwargs(self._model, **bindings)
             yield model, bindings
 
-    def query(self) -> list[_TModelInstance]:
+    def query(self, query: str | None = None) -> list[_TModelInstance]:
         """Run query against endpoint, map SPARQL result sets to model and return model instances."""
-        return [model for model, _ in self._run_query()]
+        return [model for model, _ in self._run_query(query=query)]
 
-    def query_group_by(self, group_by: str) -> dict[str, list[_TModelInstance]]:
+    def query_group_by(
+        self, group_by: str, query: str | None = None
+    ) -> dict[str, list[_TModelInstance]]:
         """Run query against endpoint and group results by a SPARQL binding."""
         group = defaultdict(list)
 
-        for model, bindings in self._run_query():
+        for model, bindings in self._run_query(query):
             try:
                 key = bindings[group_by]
             except KeyError:
@@ -110,14 +119,9 @@ class SPARQLModelAdapter(Generic[_TModelInstance]):
 
         return group
 
-    def _get_count(self) -> int:
+    def _get_count(self, query: str) -> int:
         """Construct a count query from the initialized query, run it and return the count result."""
-        count_query = construct_count_query(self.sparql_wrapper.queryString)
-
-        with temporary_query_override(self.sparql_wrapper):
-            self.sparql_wrapper.setQuery(count_query)
-            result = get_bindings_from_query_result(self.sparql_wrapper.query())
-
+        result = sparql_wrapper_query(query=query, sparql_wrapper=self.sparql_wrapper)
         return int(next(result)["cnt"])
 
     def _query_paginate_ungrouped(self, page: int, size: int) -> Page[_TModelInstance]:
@@ -131,31 +135,34 @@ class SPARQLModelAdapter(Generic[_TModelInstance]):
         paginated_query = ungrouped_pagination_base_query.substitute(
             query=self._query, offset=calculate_offset(page, size), limit=size
         )
+        count_query = construct_count_query(self._query)
 
-        total = self._get_count()
+        items = self.query(query=paginated_query)
+        total = self._get_count(count_query)
         pages = math.ceil(total / size)
-
-        with temporary_query_override(self.sparql_wrapper):
-            self.sparql_wrapper.setQuery(paginated_query)
-            items: list[_TModelInstance] = self.query()
 
         return Page(items=items, page=page, size=size, total=total, pages=pages)
 
-    @overload
-    def query_paginate(
-        self, page: int, size: int, group_by: None = None
-    ) -> Page[_TModelInstance]: ...
-
-    @overload
-    def query_paginate(
+    def _query_paginate_grouped(
         self, page: int, size: int, group_by: str
-    ) -> dict[str, list[_TModelInstance]]: ...
+    ) -> Page[_TModelInstance]:
+        grouped_pagination_query = construct_grouped_pagination_query(
+            query=self._query, page=page, size=size, group_by=group_by
+        )
+        grouped_count_query = construct_grouped_count_query(
+            query=self._query, group_by=group_by
+        )
+
+        items = self.query_group_by(group_by=group_by, query=grouped_pagination_query)
+        total = self._get_count(grouped_count_query)
+        pages = math.ceil(total / size)
+        return Page(items=items, page=page, size=size, total=total, pages=pages)
 
     def query_paginate(
         self, page: int, size: int, group_by: str | None = None
-    ) -> Page[_TModelInstance] | dict[str, list[_TModelInstance]]:
+    ) -> Page[_TModelInstance]:
         """Run query with pagination according to page and size and optional grouping."""
         if group_by is None:
             return self._query_paginate_ungrouped(page=page, size=size)
         else:
-            raise NotImplementedError
+            return self._query_paginate_grouped(page=size, size=size, group_by=group_by)
